5d33f38f439c2ee18c8bdb78ff3f8194b509c63c
SKaczmarek98 <szykacz@gmail.com>
2021-03-22 19:36:44 +0100

    test

---
+++ loT/lab1/PeopleWebApi/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeopleWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var people = new List<Person>
+            {
+                new Person
+                {
+                    FirstName = "Szymon",
+                    LastName = "Kamczarek",
+                    Id = 1
+                }
+            };
+
+            return Ok(people);
+        }
+    }
+}
+   
---
+++ loT/lab1/PeopleWebApi/PeopleDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWebApi
+{
+    public class PeopleDb : DbContext
+    {
+        public PeopleDb(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
