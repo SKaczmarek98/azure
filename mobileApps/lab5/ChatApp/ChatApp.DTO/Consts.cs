6e7ee7f3a56513f5ce38f63f7bc96fad11d59ebe
SKaczmarek98 <szykacz@gmail.com>
2021-05-07 19:12:55 +0200

    lab 5,6

---
+++ mobileApps/lab5/ChatApp/ChatApp.DTO/Consts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.DTO
+{
+    public class Consts
+    {
+        public const string RECIVE_MESSAGE = "ReceiveMessage";
+    }
+}
---
+++ mobileApps/lab5/ChatApp/ChatApp.Mobile/ChatApp.Mobile/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChatApp.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnLogin.Cliked += BtnLogin_Clicked;
+
+            private void BtnLogin_Clicked(object sender, EventArgs e)
+            {
+                var userName txtLogin.Text;
+                if(string.IsNullOrWhiteSpace(userName))
+                {
+                    await DisplayAlert("Validation errors", "The user name is required.", "Ok");
+                }
+                throw new NotImplementedException();
+            }
+
+        }
+    }
+}
